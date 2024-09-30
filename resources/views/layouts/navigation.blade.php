7e4f11bf822de0da64637b34b71f65bd5d4b7952
U5 <kyutech.joko@gmail.com>
2024-09-30 11:28:18 +0900

    文言修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('ゲーム') }}
                     </x-nav-link>
                     <x-nav-link :href="route('scores.index')" :active="request()->routeIs('scores.index')">
                         {{ __('ランキング') }}
@@ -82,7 +82,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('ゲーム') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('scores.index')" :active="request()->routeIs('scores.index')">
                 {{ __('ランキング') }}
